e6f78ac4911581e21b63b7636df921fa1d1743d9
Isar_NS <isar.nasimov98@gmail.com>
2022-12-28 14:05:26 +0200

    Create GettingStarted.sh

---
+++ GettingStarted.sh
@@ -0,0 +1,4 @@
+export SPECTRAL_DSN=<Your_DSN>
+cd path/to/test/yaml
+$HOME/.spectral/spectral scan # will send the results to specralops
+$HOME/.spectral/spectral scan --nosend # will not send the results to specralops
